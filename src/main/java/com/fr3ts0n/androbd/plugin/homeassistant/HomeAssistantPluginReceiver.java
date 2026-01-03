b426287dec460705bf71398de95bbe264b92ccc0
ian-morgan99 <106852423+ian-morgan99@users.noreply.github.com>
2026-01-03 02:25:35 +0000

    Fix: Rename PluginReceiver.java to match class name HomeAssistantPluginReceiver

---
+++ src/main/java/com/fr3ts0n/androbd/plugin/homeassistant/HomeAssistantPluginReceiver.java
@@ -0,0 +1,11 @@
+package com.fr3ts0n.androbd.plugin.homeassistant;
+
+/**
+ * Plugin broadcast receiver for Home Assistant plugin
+ */
+public class HomeAssistantPluginReceiver extends com.fr3ts0n.androbd.plugin.PluginReceiver {
+    @Override
+    public Class<?> getPluginClass() {
+        return HomeAssistantPlugin.class;
+    }
+}
